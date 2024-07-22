206cf31985a07dadd9d14f51c50cb76b3ad39431
Andrey Emelianov <621077@mail.ru>
2024-07-22 19:26:14 +0300

    реализовал шаблон для компонента SortPopup с сортировкой и TopBar в который обернул категории и сортпопап

---
+++ components/shared/sort-popup.tsx
@@ -0,0 +1,21 @@
+import { ArrowDown } from 'lucide-react';
+
+import { cn } from '@/lib/utils';
+
+interface SortPopupProps {
+  className?: string;
+}
+
+export const SortPopup: React.FC<SortPopupProps> = ({ className }) => {
+  return (
+    <div
+      className={cn(
+        'inline-flex items-center gap-1 bg-gray-50 px-5 h-[52px] rounded-2xl cursor-pointer',
+        className,
+      )}>
+      <ArrowDown size={16} />
+      <b>Сортировка:</b>
+      <b className="text-primary">популярное</b>
+    </div>
+  );
+};
