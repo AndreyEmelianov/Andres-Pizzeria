30662950f0ee54bbb6bc7052cebf457fad762f28
Andrey Emelianov <621077@mail.ru>
2024-07-22 17:33:10 +0300

    добавил в зависимости ui элементы из shadcn, бибилиотеку с иконками, clsx, анимацию, тайпскрипт; Добавил нужный шрифт, проинициализировал ui компоненты из библиотек

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return <h1>Start project</h1>;
+  return (
+    <h1>
+      Start project
+      <Button variant={'outline'}>Start</Button>
+    </h1>
+  );
 }
