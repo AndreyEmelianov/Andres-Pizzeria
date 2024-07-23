8288c907abb973f0c893f72abcbb6b257fcd004f
Andrey Emelianov <621077@mail.ru>
2024-07-23 16:38:41 +0300

    реализовал компонент RangeSlider на основе слайдера из библиотке radix-ui и добавил его в компонент filters

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,4 +1,6 @@
+import { Input } from '../ui';
 import { FilterCheckbox } from './filter-checkbox';
+import { RangeSlider } from './range-slider';
 import { Title } from './title';
 
 interface FiltersProps {
@@ -9,9 +11,20 @@ export const Filters: React.FC<FiltersProps> = ({ className }) => {
   return (
     <div className={className}>
       <Title text="Фильтрация" size="sm" className="mb-5 font-bold" />
+
       <div className="flex flex-col gap-4">
         <FilterCheckbox text="Можно собирать" value="1" />
         <FilterCheckbox text="Новинки" value="2" />
+      </div>
+
+      <div className="mt-5 border-y border-y-neutral-200 py-6 pb-7">
+        <p className="font-bold mb-3">Цена от и до:</p>
+        <div className="flex gap-3 mb-5">
+          <Input type="number" placeholder="0" min={0} max={3000} defaultValue={0} />
+          <Input type="number" placeholder="3000" min={100} max={3000} />
+        </div>
+
+        <RangeSlider min={0} max={3000} step={10} value={[0, 3000]} />
       </div>
     </div>
   );
