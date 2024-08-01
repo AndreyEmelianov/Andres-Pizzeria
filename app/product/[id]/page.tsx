7978c0cccf9b46524e805f8802e4535ebab1043c
Andrey Emelianov <621077@mail.ru>
2024-08-01 13:39:16 +0300

    добавил изображение и выбор вариантов пиццы в хардкодед значениях на странице товара, чтобы страница пока не была пустой

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,3 +1,44 @@
-export default function ProductPage({ params: { id } }: { params: { id: string } }) {
-  return <div>Товар с айди{id}</div>;
+import { notFound } from 'next/navigation';
+
+import { prisma } from '@/prisma/prisma-client';
+import { Container, ProductImage, Title } from '@/components/shared';
+import { ToggleVariants } from '@/components/shared/toggle-variants';
+
+export default async function ProductPage({ params: { id } }: { params: { id: string } }) {
+  const product = await prisma.product.findFirst({ where: { id: Number(id) } });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <Container className="flex flex-col my-10">
+      <div className="flex flex-1">
+        <ProductImage imageUrl={product.imageUrl} size={30} />
+
+        <div className="w-[490px] bg-[#fcfcfc] p-7">
+          <Title text={product.name} size="md" className="font-extrabold mb-1" />
+          <p className="text-gray-400">info about pizza</p>
+          <ToggleVariants
+            selectedValue="1"
+            items={[
+              {
+                name: 'Маленькая',
+                value: '1',
+              },
+              {
+                name: 'Средняя',
+                value: '2',
+              },
+              {
+                name: 'Большая',
+                value: '3',
+                disabled: true,
+              },
+            ]}
+          />
+        </div>
+      </div>
+    </Container>
+  );
 }
